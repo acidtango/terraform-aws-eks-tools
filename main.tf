3577a2f85165baeab92b21d63132e3c30bc08519
Abel Garcia Dorta <abelgarcia@acidtango.com>
2021-09-13 14:30:02 +0100

    refactor: providers should be on user side not on module side

--- main.tf
+++ main.tf
@@ -6,24 +6,6 @@ data "aws_eks_cluster" "eks-cluster" {
   name = var.eks_cluster_name
 }
 
-data "aws_eks_cluster_auth" "aws_iam_authenticator" {
-  name = data.aws_eks_cluster.eks-cluster.name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks-cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks-cluster.endpoint
-    token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-  }
-}
-
 
 // AWS Load Balancer Controller Installation
 
