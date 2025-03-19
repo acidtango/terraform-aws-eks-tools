b4759e94942a2049609df10055808dec684f55da
Manuel Escalante <maescalantehe@gmail.com>
2025-03-18 22:50:14 -0400

    chore: update Terraform and provider version requirements and update .gitignore

--- versions.tf
+++ versions.tf
@@ -1,33 +1,25 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = "1.11.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.3"
+      version = "~> 5.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.2"
-    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.3"
+      version = "~> 2.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.1"
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.1"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1"
-    }
+
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "~> 1.14"
+      version = "~> 1.0"
     }
   }
 }
