58f5f6aeae08938c0b2f6d61bd3e8915a072bc48
Abel Garcia Dorta <abelgarcia@acidtango.com>
2025-05-05 10:50:56 +0100

    feat: reimplement 1.32 but without breaking changes
    Co-authored-by: Manuel Escalante <maescalantehe@gmail.com>

--- main.tf
+++ main.tf
@@ -13,62 +13,205 @@ locals {
   }
 }
 
+
 // AWS Load Balancer Controller Installation
 
-module "alb_controller" {
-  source  = "Young-ook/eks/aws//modules/lb-controller"
-  version = "1.7.10"
+resource "aws_iam_role" "aws_lb_controller_role" {
+  name = "${var.eks_cluster_name}-aws-lb-controller-role"
 
-  oidc = local.oidc
-  helm = {
-    vars = {
-      clusterName = var.eks_cluster_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = var.iam_oidc_provider_arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(var.iam_oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_policy" "aws_lb_controller_policy" {
+  name   = "${var.eks_cluster_name}-AWSLoadBalancerControllerIAMPolicy"
+  policy = file("${path.module}/iam_policy.json")
+}
+
+
+resource "aws_iam_role_policy_attachment" "aws_lb_controller_policy_attach" {
+  role       = aws_iam_role.aws_lb_controller_role.name
+  policy_arn = aws_iam_policy.aws_lb_controller_policy.arn
+}
+
+
+
+
+resource "kubernetes_service_account" "aws_lb_controller_sa" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.aws_lb_controller_role.arn
     }
   }
 }
 
 
+resource "helm_release" "alb_ingress_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.12.0"
+
+  values = [
+    <<-EOT
+    clusterName: ${var.eks_cluster_name}
+    serviceAccount:
+      create: false
+      name: ${kubernetes_service_account.aws_lb_controller_sa.metadata[0].name}
+      annotations:
+        eks.amazonaws.com/role-arn: ${aws_iam_role.aws_lb_controller_role.arn}
+    vpcId: ${data.aws_eks_cluster.eks-cluster.vpc_config[0].vpc_id}
+    region: ${data.aws_region.current.name}
+    EOT
+  ]
+}
+
+
 // AWS External DNS Ingress Controller Installation
 
-module "external_dns" {
-  source  = "lablabs/eks-external-dns/aws"
-  version = "1.1.1"
+resource "aws_iam_role" "aws_external_dns_role" {
+  name = "${var.eks_cluster_name}-aws-external-dns-role"
 
-  cluster_identity_oidc_issuer     = data.aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
-  cluster_identity_oidc_issuer_arn = var.iam_oidc_provider_arn
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = var.iam_oidc_provider_arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(var.iam_oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:kube-system:external-dns"
+        }
+      }
+    }]
+  })
 
-  settings = {
-    "domainFilters[0]" = var.domain
+  tags = var.tags
+}
+
+resource "aws_iam_policy" "aws_external_dns_policy" {
+  name = "${var.eks_cluster_name}-AllowExternalDNSUpdates"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ChangeResourceRecordSets"
+        ]
+        Resource = "arn:aws:route53:::hostedzone/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ListHostedZones",
+          "route53:ListResourceRecordSets",
+          "route53:ListTagsForResource"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "aws_external_dns_policy_attach" {
+  role       = aws_iam_role.aws_external_dns_role.name
+  policy_arn = aws_iam_policy.aws_external_dns_policy.arn
+}
+
+
+resource "kubernetes_service_account" "aws_external_dns_sa" {
+  metadata {
+    name      = "external-dns"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.aws_external_dns_role.arn
+    }
   }
+}
+
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  version    = "1.15.2"
+
+  values = [
+    <<-EOT
+    txtOwnerId: "${var.eks_cluster_name}"
+    domainFilters:
+      - "${var.domain}"
+    policy: sync
+    logLevel: debug
+    sources:
+      - ingress
+      - service
+    serviceAccount:
+      create: false
+      name: ${kubernetes_service_account.aws_external_dns_sa.metadata[0].name}
+      annotations:
+        eks.amazonaws.com/role-arn: ${aws_iam_role.aws_external_dns_role.arn}
+    EOT
+  ]
 }
 
 
 // AWS Container Inshights Installation
 
-module "container-insights" {
-  source  = "Young-ook/eks/aws//modules/container-insights"
-  version = "1.7.10"
-
-  cluster_name = data.aws_eks_cluster.eks-cluster.name
-  oidc         = local.oidc
-  features = {
-    enable_metrics = var.enable-metrics
-    enable_logs    = var.enable-logs
-  }
+resource "helm_release" "container_insights_logs" {
+  count      = var.enable-logs ? 1 : 0
+  name       = "container-insights-logs"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-for-fluent-bit"
+  version    = "0.1.34"
 }
+
+// This helm is not working
+#resource "helm_release" "container_insights_metrics" {
+#  count      = var.enable-metrics ? 1 : 0
+#  name       = "container-insights-metrics"
+#  namespace  = "kube-system"
+#  repository = "https://aws.github.io/eks-charts"
+#  chart      = "aws-cloudwatch-metrics"
+#  version    = "0.0.11"
+#  #values = [
+#  #  <<-EOT
+#  #  clusterName: "${var.eks_cluster_name}"
+#  #  EOT
+#  #]
+#}
 
 
 // Metrics server Installation (for Horizontal Pod Autoscaler)
 
-module "metrics-server" {
-  source  = "Young-ook/eks/aws//modules/metrics-server"
-  version = "1.7.10"
-
-  oidc = local.oidc
-  helm = {
-    repository = "https://kubernetes-sigs.github.io/metrics-server/"
-    version    = "3.8.2"
-  }
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.12.2"
 }
 
 
@@ -77,156 +220,159 @@ module "metrics-server" {
 
 module "karpenter" {
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version = "19.5.1"
+  version = "20.33.1"
 
   cluster_name = var.eks_cluster_name
+
+  # This variables change to true (by default) in the next breaking changes
+  enable_v1_permissions           = true
+  create_pod_identity_association = true
 
   irsa_oidc_provider_arn          = var.iam_oidc_provider_arn
   irsa_namespace_service_accounts = ["karpenter:karpenter"]
 
-  # Since Karpenter is running on an EKS Managed Node group,
-  # we can re-use the role that was created for the node group
-  create_iam_role = false
-  iam_role_arn    = var.eks-node-group-iam-role-arn
+  create_node_iam_role          = false
+  node_iam_role_arn             = var.eks-node-group-iam-role-arn
+  node_iam_role_use_name_prefix = false
 
-  create_irsa = true
-  # due to names too long we need to provide short ones
-  irsa_name = substr("KarpenterIRSA-${var.eks_cluster_name}", 0, 37)
-}
+  # Error: creating EKS Access Entry ResourceInUseException: The specified access entry resource is already in use on this cluster.
+  # https://github.com/terraform-aws-modules/terraform-aws-eks/blob/master/docs/UPGRADE-20.0.md#authentication_mode--api_and_config_map
+  create_access_entry = false
 
-provider "aws" {
-  region = "us-east-1"
-  alias  = "virginia"
-}
+  enable_irsa             = true
+  create_instance_profile = true
 
-data "aws_ecrpublic_authorization_token" "token" {
-  provider = aws.virginia
+  iam_role_use_name_prefix = false
+  iam_role_name            = "KarpenterIRSA-${var.eks_cluster_name}"
+  iam_role_description     = "Karpenter IAM role for service account"
+  iam_policy_name          = "KarpenterIRSA-${var.eks_cluster_name}"
+  iam_policy_description   = "Karpenter IAM role for service account"
+
+  tags = var.tags
 }
 
 resource "helm_release" "karpenter" {
   namespace        = "karpenter"
   create_namespace = true
 
-  name                = "karpenter"
-  repository          = "oci://public.ecr.aws/karpenter"
-  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  repository_password = data.aws_ecrpublic_authorization_token.token.password
-  chart               = "karpenter"
-  version             = "v0.24.0"
+  name       = "karpenter"
+  repository = "oci://public.ecr.aws/karpenter"
+  chart      = "karpenter"
+  version    = "1.3.0"
 
-  set {
-    name  = "settings.aws.clusterName"
-    value = var.eks_cluster_name
-  }
 
-  set {
-    name  = "settings.aws.clusterEndpoint"
-    value = data.aws_eks_cluster.eks-cluster.endpoint
-  }
-
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.karpenter.irsa_arn
-  }
-
-  set {
-    name  = "settings.aws.defaultInstanceProfile"
-    value = module.karpenter.instance_profile_name
-  }
-
-  set {
-    name  = "settings.aws.interruptionQueueName"
-    value = module.karpenter.queue_name
-  }
+  values = [
+    <<-EOT
+    settings:
+      clusterName: ${var.eks_cluster_name}
+      clusterEndpoint: ${data.aws_eks_cluster.eks-cluster.endpoint}
+      defaultInstanceProfile: ${module.karpenter.instance_profile_name}
+      interruptionQueueName: ${module.karpenter.queue_name}
+    serviceAccount:
+      annotations:
+        eks.amazonaws.com/role-arn: ${module.karpenter.iam_role_arn}
+    EOT
+  ]
 }
 
-# Workaround - https://github.com/hashicorp/terraform-provider-kubernetes/issues/1380#issuecomment-967022975
-# Use all instance types as fallback
-resource "kubectl_manifest" "karpenter_provisioner" {
+resource "kubectl_manifest" "karpenter_nodeclass_default" {
   yaml_body = <<-YAML
-    apiVersion: karpenter.sh/v1alpha5
-    kind: Provisioner
+    apiVersion: karpenter.k8s.aws/v1
+    kind: EC2NodeClass
     metadata:
       name: default
     spec:
-      consolidation:
-        enabled: true
-      weight: 1
-      ttlSecondsUntilExpired: 604800
-      requirements:
-        - key: karpenter.k8s.aws/instance-category
-          operator: In
-          values: ["t", "c", "m", "r"]
-# Blocklist for instance types that give us problems in the past
-        - key: node.kubernetes.io/instance-type
-          operator: NotIn
-          values: ["m1.small"]
-        - key: karpenter.sh/capacity-type
-          operator: In
-          values: ["spot", "on-demand"]
-        - key: kubernetes.io/arch
-          operator: In
-          values: ["amd64"]
-      limits:
-        resources:
-          cpu: 64
-      provider:
-        subnetSelector:
-          karpenter.sh/discovery: "true"
-        securityGroupSelector:
-          aws-ids: ${data.aws_eks_cluster.eks-cluster.vpc_config[0].cluster_security_group_id}
-        tags:
-          karpenter.sh/discovery: ${var.eks_cluster_name}
+      amiSelectorTerms:
+        - alias: al2023@latest
+      subnetSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: "true"
+      securityGroupSelectorTerms:
+        - id: ${data.aws_eks_cluster.eks-cluster.vpc_config[0].cluster_security_group_id}
+      role: "${var.eks-node-group-iam-role-arn}"
+      tags:
+        "Name": "karpenter-node"
+        "karpenter.sh/discovery": "true"
   YAML
 
-  depends_on = [
-    helm_release.karpenter
-  ]
+  depends_on = [helm_release.karpenter]
 }
 
-# Workaround - https://github.com/hashicorp/terraform-provider-kubernetes/issues/1380#issuecomment-967022975
-# provisioner with cheapest instance types
-resource "kubectl_manifest" "karpenter_provisioner_cheap" {
+resource "kubectl_manifest" "karpenter_nodepool_default" {
   yaml_body = <<-YAML
-    apiVersion: karpenter.sh/v1alpha5
-    kind: Provisioner
+    apiVersion: karpenter.sh/v1
+    kind: NodePool
+    metadata:
+      name: default
+    spec:
+      weight: 20
+      template:
+        spec:
+          expireAfter: 168h
+          nodeClassRef:
+            group: karpenter.k8s.aws
+            kind: EC2NodeClass
+            name: default
+          requirements:
+            - key: karpenter.k8s.aws/instance-category
+              operator: In
+              values: ["t", "c", "m", "r"]
+            - key: node.kubernetes.io/instance-type
+              operator: NotIn
+              values: ["m1.small"]
+            - key: karpenter.sh/capacity-type
+              operator: In
+              values: ["spot", "on-demand"]
+            - key: kubernetes.io/arch
+              operator: In
+              values: ["amd64"]
+      limits:
+          cpu: "32"
+      disruption:
+        consolidationPolicy: WhenEmptyOrUnderutilized
+        consolidateAfter: 5m
+  YAML
+
+  depends_on = [kubectl_manifest.karpenter_nodeclass_default]
+}
+
+resource "kubectl_manifest" "karpenter_nodepool_cheap" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1
+    kind: NodePool
     metadata:
       name: cheap-instances
     spec:
-      consolidation:
-        enabled: true
-      weight: 100
-      ttlSecondsUntilExpired: 604800
-      requirements:
-        - key: karpenter.k8s.aws/instance-category
-          operator: In
-          values: ["t", "c", "m", "r"]
-        - key: karpenter.k8s.aws/instance-size
-          operator: In
-          values: ["nano", "micro", "small", "medium"]
-# Blocklist for instance types that give us problems in the past
-        - key: node.kubernetes.io/instance-type
-          operator: NotIn
-          values: ["m1.small"]
-        - key: karpenter.sh/capacity-type
-          operator: In
-          values: ["spot", "on-demand"]
-        - key: kubernetes.io/arch
-          operator: In
-          values: ["amd64"]
+      weight: 50
+      template:
+        spec:
+          expireAfter: 168h
+          nodeClassRef:
+            group: karpenter.k8s.aws
+            kind: EC2NodeClass
+            name: default
+          requirements:
+            - key: karpenter.k8s.aws/instance-category
+              operator: In
+              values: ["t", "c", "m", "r"]
+            - key: karpenter.k8s.aws/instance-size
+              operator: In
+              values: ["nano", "micro", "small", "medium"]
+            - key: node.kubernetes.io/instance-type
+              operator: NotIn
+              values: ["m1.small"]
+            - key: karpenter.sh/capacity-type
+              operator: In
+              values: ["spot", "on-demand"]
+            - key: kubernetes.io/arch
+              operator: In
+              values: ["amd64"]
       limits:
-        resources:
-          cpu: 64
-      provider:
-        subnetSelector:
-          karpenter.sh/discovery: "true"
-        securityGroupSelector:
-          aws-ids: ${data.aws_eks_cluster.eks-cluster.vpc_config[0].cluster_security_group_id}
-        tags:
-          karpenter.sh/discovery: ${var.eks_cluster_name}
+          cpu: "32"
+      disruption:
+        consolidationPolicy: WhenEmptyOrUnderutilized
+        consolidateAfter: 5m
   YAML
 
-  depends_on = [
-    helm_release.karpenter
-  ]
+  depends_on = [kubectl_manifest.karpenter_nodeclass_default]
 }
--- versions.tf
+++ versions.tf
@@ -13,18 +13,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.3"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.1"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.1"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14"
