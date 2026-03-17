49b2a9853de798b55c7124ce248c94e5ce1a21cb
Manuel Escalante <maescalantehe@gmail.com>
2026-03-17 15:46:15 -0400

    feat: add log retention variable and improve variable descriptions (#40)

--- variables.tf
+++ variables.tf
@@ -1,34 +1,49 @@
-variable "eks_cluster_name" {
-  description = "eks cluster name where you want to install this tools"
+variable "domain" {
+  description = "Domain for ExternalDNS to listen for changes"
   type        = string
 }
 
-variable "iam_oidc_provider_arn" {
-  description = "identity issuer of eks cluster you want to install external dns"
-}
-
-variable "iam_oidc_provider_url" {
-  description = "identity issuer of eks cluster you want to install some tools url"
-}
-
-variable "domain" {
-  description = "domain for external dns to listen for changes"
-}
-
-variable "enable_metrics" {
-  description = "A conditional indicator to enable container insights metrics"
-  type        = bool
-  default     = true
-}
-
-variable "enable_logs" {
-  description = "A conditional indicator to enable container insights logs"
-  type        = bool
-  default     = true
+variable "eks_cluster_name" {
+  description = "EKS cluster name where tools will be installed"
+  type        = string
 }
 
 variable "eks_node_group_iam_role_arn" {
-  description = "karpenter needs the node group iam role arn to create new nodes"
+  description = "Node group IAM role ARN required by Karpenter to create new nodes"
+  type        = string
+}
+
+variable "enable_logs" {
+  description = "Enable Container Insights logs via Fluent Bit"
+  type        = bool
+  default     = true
+}
+
+variable "enable_metrics" {
+  description = "Enable Container Insights metrics via CloudWatch Agent"
+  type        = bool
+  default     = true
+}
+
+variable "iam_oidc_provider_arn" {
+  description = "OIDC provider ARN for the EKS cluster"
+  type        = string
+}
+
+variable "iam_oidc_provider_url" {
+  description = "OIDC provider URL for the EKS cluster"
+  type        = string
+}
+
+variable "log_retention_in_days" {
+  description = "Retention period in days for CloudWatch log group"
+  type        = number
+  default     = 90
+
+  validation {
+    condition     = contains([0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1096, 1827, 2192, 2557, 2922, 3288, 3653], var.log_retention_in_days)
+    error_message = "log_retention_in_days must be one of: 0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1096, 1827, 2192, 2557, 2922, 3288, 3653."
+  }
 }
 
 variable "tags" {
